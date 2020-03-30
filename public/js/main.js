b1fa97c6582323cf98b39f44be85d91e1db9fa6e
Roberto R.A <roberto.roccoangeloni@gmail.com>
2020-03-30 14:16:23 +0200

    added typescript files

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+function greeter(person) {
+    return "Ciao " + person.firstName + " " + person.lastName;
+}
+var user = { firstName: "Lippo", lastName: "Lippi" };
+document.body.textContent = greeter(user);
+//# sourceMappingURL=main.js.map
