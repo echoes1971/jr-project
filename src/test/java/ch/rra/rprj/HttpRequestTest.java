db395fffeac41a4bb33cd3dfc9789a317b2b3050
Roberto Rocco-Angeloni <roberto.rocco@nagra.com>
2024-03-06 15:44:31 +0100

    ...

--- src/test/java/ch/rra/rprj/HttpRequestTest.java
+++ src/test/java/ch/rra/rprj/HttpRequestTest.java
@@ -1,9 +1,30 @@
 package ch.rra.rprj;
 
+import org.testng.annotations.Test;
+
+import java.util.LinkedHashMap;
+
 public class HttpRequestTest {
 
-
+    @Test
     void test_doget() {
+        HttpRequest req = new HttpRequest();
+
+        LinkedHashMap params = new LinkedHashMap();
+
+        LinkedHashMap headers = new LinkedHashMap();
+
+        String body = "{method:'ping',params:[]}";
+
+        LinkedHashMap hm = new LinkedHashMap();
+        hm.put("url","https://www.roccoangeloni.it/jsonserver.php");
+        hm.put("method","GET");
+        hm.put("params",params);
+        hm.put("headers",headers);
+        hm.put("contentType","application/json");
+        hm.put("body",body);
+
+        req.stepHttpRequest(hm);
 
     }
 
